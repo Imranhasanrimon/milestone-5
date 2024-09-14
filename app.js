a7c463974993707b73e96244873136df2ccb1600
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-14 21:21:19 +0600

    id class and tag selectior is done

---
+++ app.js
@@ -0,0 +1,8 @@
+const liCollection = document.getElementsByTagName('li');
+for (let i of liCollection) {
+    console.log(i);
+
+}
+// console.log(document.getElementsByTagName('h1').innetText);
+const h1Manipulation = document.getElementById('first').innerText = 'DOM'
+console.log(h1Manipulation);
