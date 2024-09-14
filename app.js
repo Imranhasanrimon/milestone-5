b215b46612c52d165f22c88a8e5464b06c49ae01
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-14 21:37:01 +0600

    query selector

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const liCollection = document.getElementsByTagName('li');
-for (let i of liCollection) {
-    console.log(i);
+// const liCollection = document.getElementsByTagName('li');
+// for (let i of liCollection) {
+//     console.log(i);
 
-}
-// console.log(document.getElementsByTagName('h1').innetText);
-const h1Manipulation = document.getElementById('first').innerText = 'DOM'
-console.log(h1Manipulation);
+// }
+// // console.log(document.getElementsByTagName('h1').innetText);
+// const h1Manipulation = document.getElementById('first').innerText = 'DOM'
+// console.log(h1Manipulation);
+// const mainName = document.getElementsByClassName('main-name');
+// console.log(mainName);
+const query = document.querySelectorAll('.parent li');
+console.log(query);
