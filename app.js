2f50e1544a364e009ee9bc71f314676ca61e7e58
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-19 00:06:16 +0600

    form validation

--- app.js
+++ app.js
@@ -343,3 +343,14 @@
 // })
 
 /*         MODULE 27          */
+document.getElementById('login-btn').addEventListener('click', function (event) {
+    event.preventDefault()
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if (phoneNumber === '01755' && pinNumber === '1234') {
+        console.log('you are logged in ');
+    } else {
+        console.log('wrong pass or phone');
+    }
+})
