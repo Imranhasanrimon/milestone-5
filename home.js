d89d17a231202d899c38afd95eb820a18c57609a
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-19 16:23:10 +0600

    adding money to the existing balance

--- home.js
+++ home.js
@@ -1,6 +1,13 @@
 document.getElementById('btn-add-money').addEventListener('click', function (event) {
     event.preventDefault();
-    const amount = document.getElementById('input-add-money').value;
+    const amount = Number(document.getElementById('input-add-money').value);
     const pin = document.getElementById('input-pin-number').value;
-    console.log(amount, pin);
+
+    if (pin === '1234') {
+        const balance = Number(document.getElementById('balance').innerText);
+        document.getElementById('balance').innerText = balance + amount;
+        console.log('money added');
+    } else {
+        alert('failed to add money');
+    }
 })
