15db09145098fbbb171aa6d5fa1146ba210c936d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-19 19:13:59 +0600

    cash out part is done

--- home.js
+++ home.js
@@ -10,3 +10,18 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
         alert('failed to add money');
     }
 })
+
+
+//this is for cash out
+document.getElementById('btn-cashOut').addEventListener('click', function (event) {
+    event.preventDefault();
+    const amountCashOut = parseFloat(document.getElementById('input-cashout').value);
+    const currenBalance = parseFloat(document.getElementById('balance').innerText);
+    if (document.getElementById('input-pin-cashout').value === '1234') {
+        document.getElementById('balance').innerText = currenBalance - amountCashOut;
+
+    } else {
+        alert('wrong password')
+    }
+    console.log(amountCashOut, currenBalance);
+})
