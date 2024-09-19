19bee7ace24b0e0d9123106314c16b4d061d49ce
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-19 16:06:35 +0600

    getting input value

--- home.js
+++ home.js
@@ -1,1 +1,6 @@
-console.log('afsjfs');
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+    const amount = document.getElementById('input-add-money').value;
+    const pin = document.getElementById('input-pin-number').value;
+    console.log(amount, pin);
+})
